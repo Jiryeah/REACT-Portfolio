55d166f16695444f608b261048e82620c073839f
Chidori <business.faissal@outlook.com>
2022-03-27 23:20:30 -0500

    about me drafted

---
+++ src/components/About/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section>
+      <h1>Personal Development</h1>
+      /* Rough Draft */
+      <p>
+        Hello! My name is Faissal Shabani and welcome to my portfolio. I'll assume that my name has
+        you wondering where I come from, so I'll assist you with that. I was born in the Democratic
+        Republic of Congo, formerly Zaire, and came to the states as a refugee due to the Rwandan
+        Genocide of '94 and the First and Second Congo Wars. Now, my love for technology mixed with
+        a ferocious sense of stubbornness has led me to fall in love with Web Dev. I've found that
+        Web Dev allows me to create and use my pursuit of perfection for a meaningful cause. Please
+        allow this portfolio to illustrate some of my work and the passion that I have for
+        development. Cheers!
+      </p>
+    </section>
+  );
+};
