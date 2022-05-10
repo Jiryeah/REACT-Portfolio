6b270c0be6a6dcfc8f304e626530c83d12a10d46
Chidori <business.faissal@outlook.com>
2022-05-09 19:19:05 -0500

    added social media icons

--- client/src/components/NavigationDots.jsx
+++ client/src/components/NavigationDots.jsx
@@ -1,3 +1,5 @@
+import React from 'react';
+
 const NavigationDots = ({ active }) => (
   <div className="app__navigation">
     {['home', 'about', 'work', 'skills', 'testimonial', 'contact'].map((item, index) => (
---
+++ client/src/components/SocialMedia.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BsTwitter, BsGithub } from 'react-icons/bs';
+import { FaLinkedin } from 'react-icons/fa';
+
+const SocialMedia = () => (
+  <div className="app__social">
+    <div>
+    </div>
+      <BsGithub />
+    <div>
+      <FaLinkedin />
+    </div>
+    <div>
+      <BsTwitter />
+    </div>
+  </div>
+);
+
+export default SocialMedia;
