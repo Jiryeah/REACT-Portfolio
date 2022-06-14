abbd2f5ab30518fd0765b18dd1fabf66e29af299
Chidori <business.faissal@outlook.com>
2022-06-14 13:09:03 -0500

    left div almost done

---
+++ portfolio/components/Contact.jsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import { FaLinkedinIn, FaGithub } from 'react-icons/fa';
+import { AiOutlineMail } from 'react-icons/ai';
+import { BsFillPersonLinesFill } from 'react-icons/bs';
+
+const Contact = () => {
+  return (
+    <div className='w-full lg:h-screen'>
+      <div className='max-w-[1240px] m-auto px-2 py-16 w-full'>
+        <p className='text-xl tracking-widest uppercase text-blue-500'>Contact</p>
+        <h2 className='py-4'>Get In Touch</h2>
+        <div className='grid lg:grid-cols-5 gap-8'>
+          {/* left */}
+          <div className='col-span-3 lg:col-span-2 w-full h-full shadow-xl shadow-gray-400 rounded-xl p-4'>
+            <div className='lg:p-4 h-full'>
+              <div>
+                <img
+                  className='rounded-xl hover:scale-105 ease-in duration-300'
+                  src='https://picsum.photos/id/486/600'
+                  alt='/'
+                />
+              </div>
+              <div>
+                <h2 className='py-2'>Faissal Shabani</h2>
+                <p>Full-Stack Developer</p>
+                <p className='py-4'>
+                  <span className='text-blue-500'>Availability</span>: freelance or full-time
+                  positions.
+                  <br />
+                  Contact me and let's talk.
+                </p>
+              </div>
+              <div>
+                <p className='uppercase pt-8'>Connect with Me</p>
+                <div className='flex items-center justify-between py-4'>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <FaLinkedinIn />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <FaGithub />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <AiOutlineMail />
+                  </div>
+                  <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
+                    <BsFillPersonLinesFill />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* right */}
+          <div className='col-span-3 w-full h-auto shadow-xl shadow-gray-400 rounded-xl lg:p-4'>
+            <div className='p-4'>
+              <form>
+                <div className='grid md:grid-cols-2 gap-4 w-full py-2'>
+                  <div className='flex flex-col'>
+                    <label className='uppercase text-sm py-2'>Name</label>
+                    <input className='border-2 rounded-lg p-3 flex border-gray-300' type='text'
+
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
