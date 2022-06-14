99fe03453844dbb2698f85aeb31a7f60a26763b8
Chidori <business.faissal@outlook.com>
2022-06-13 20:09:44 -0500

    started added projects

--- portfolio/components/Skills.jsx
+++ portfolio/components/Skills.jsx
@@ -20,7 +20,7 @@ import Spring from '../public/assets/skills/spring-framework-icon.png';
 const Skills = () => {
   return (
     <div className='w-full lg:h-screen p-2'>
-      <div className='max-w-[1240px] flex flex-col justify-center h-full'>
+      <div className='max-w-[1240px] mx-auto flex flex-col justify-center h-full'>
         <p className='text-xl tracking-widest uppercase text-blue-500'>Skills</p>
         <h2 className='py-4'>What I Can Do</h2>
         <div className='grid md:grid-cols-2 lg:grid-cols-4 gap-8'>
