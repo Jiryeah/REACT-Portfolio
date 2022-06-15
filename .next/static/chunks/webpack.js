8deb275ec7ca1a94b09c7c85a1c2249e13086dc7
Chidori <business.faissal@outlook.com>
2022-06-15 17:14:23 -0500

    file structure

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "af66d7a9c179a8bf"; }
+/******/ 		__webpack_require__.h = function() { return "4d2612807eefb093"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- .next/static/webpack/webpack.af914c64e0a09fc1.hot-update.js
+++ .next/static/webpack/webpack.af914c64e0a09fc1.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "a4137e6303a2626c"; }
+/******/ 	__webpack_require__.h = function() { return "4d2612807eefb093"; }
 /******/ }();
 /******/ 
 /******/ /* webpack/runtime/compat */
