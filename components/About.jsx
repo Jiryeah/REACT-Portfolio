3977ffaa2d41b8c05a698824ce4c6338f760d5f8
Chidori <business.faissal@outlook.com>
2022-06-21 16:39:42 -0500

    fixed my about me section

--- components/About.jsx
+++ components/About.jsx
@@ -14,24 +14,15 @@ const About = () => {
             The <span className='text-black'>thrill</span> of problem solving
           </p>
           <p className='py-2 text-gray-600'>
-            Many in my generation, if they remember, can admit to manipulating their first bit of
-            HTML on old myspace profiles. I was the kid in the neighborhood who loved to do that for
-            myself, and others. I didn't quite understand what I was doing, but it was far from
-            programming. With a nack for problem solving, and a passion for technology, it wasn't
-            long before I decided to pursue development in it's entirety. I have since given up
-            myspace HTML edits for databases in SQL and API's in Spring Boot. I can now do more with
-            a website appearance through React, than I could have ever imagined. The same curiosity
-            that drew me into this field, has humbled me by it's never ending evolution in knowledge
-            and growth. I love programming because I can solve problems that can benefit many.
-          </p>
-          <br />
-          <p className='py-2 text-gray-600'>
-            I have been able to make interactive websites that function on all devices and platform
-            by way of the MERN stack. However, I have since learned that back-end logic, seems to be
-            my strong suit; so, I learned Java and started to use the Spring Framework. Databases
-            and API's just make sense to my brain in the same way advanced CSS concepts make sense
-            to the awesome Front-end developers out there. I intend to solidify my back-end
-            foundational skills by learning: AWS, Docker, and also TypeScript.
+            I received a certificate in Full Stack Web Development from the University of Texas at
+            San Antonio. I have been able to make interactive websites that function on all devices
+            and platforms through utilizing the MERN stack. However, I have since learned that
+            back-end logic, seems to be my strong suit; therefore, I learned Java and began to use
+            Spring Boot to further develop my "tool box". The same curiosity that drew me into this
+            field, has humbled me by it's never ending evolution in knowledge and growth. I intend
+            to solidify my back-end foundational skills by learning: AWS, Docker, and TypeScript.
+            One must never abstain from learning, because our time is finite while knowledge is
+            infinite.
           </p>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
