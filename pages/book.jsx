03c4817d5a542e6eea44f368ec7a7daea8123b76
Faissal Shabani <92201576+ZairesDev@users.noreply.github.com>
2022-07-12 12:18:22 -0500

    Updated GitHub repo link

--- pages/book.jsx
+++ pages/book.jsx
@@ -31,7 +31,7 @@ const book = () => {
           <a href='https://google-api-books-search.netlify.app/' target='_blank' rel='noreferrer'>
             <button className='px-8 py-2 mt-4 mr-8'>Demo</button>
           </a>
-          <a href='https://github.com/Jiryeah/MERN_GQL' target='_blank' rel='noreferrer'>
+          <a href='https://github.com/ZairesDev/MERN_GQL' target='_blank' rel='noreferrer'>
             <button className='px-8 py-2 mt-4'>Source Code</button>
           </a>
         </div>
