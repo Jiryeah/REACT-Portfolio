96e330cb22430ba9f51beba479a5f55af8e4b99b
Faissal Shabani <92201576+ZairesDev@users.noreply.github.com>
2022-07-12 12:17:57 -0500

    Update blog.jsx

--- pages/blog.jsx
+++ pages/blog.jsx
@@ -30,7 +30,7 @@ const blog = () => {
           <a href='https://cc-java-blog-api.herokuapp.com/' target='_blank' rel='noreferrer'>
             <button className='px-8 py-2 mt-4 mr-8'>Demo</button>
           </a>
-          <a href='https://github.com/Jiryeah/tech-news-java-api' target='_blank' rel='noreferrer'>
+          <a href='https://github.com/ZairesDev/tech-news-java-api' target='_blank' rel='noreferrer'>
             <button className='px-8 py-2 mt-4'>Source Code</button>
           </a>
         </div>
