bab4c1282d8fbeb982cb0d1af0481860cf8fdb59
Faissal Shabani <92201576+ZairesDev@users.noreply.github.com>
2022-07-12 12:13:51 -0500

    updated GitHub repo link

--- components/Main.jsx
+++ components/Main.jsx
@@ -31,7 +31,7 @@ const Main = () => {
                 <FaLinkedinIn />
               </div>
             </a>
-            <a href='https://github.com/Jiryeah' target='_blank' rel='noreferrer'>
+            <a href='https://github.com/ZairesDev' target='_blank' rel='noreferrer'>
               <div className='rounded-full shadow-lg shadow-gray-400 p-6 cursor-pointer hover:scale-110 ease-in duration-300'>
                 <FaGithub />
               </div>
