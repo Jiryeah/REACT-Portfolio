71a9397cebf6493e3bfa3ff718132928d180c2b0
Faissal Shabani <92201576+ZairesDev@users.noreply.github.com>
2022-07-12 12:17:36 -0500

    Updated GitHub repo link

--- pages/assurance.jsx
+++ pages/assurance.jsx
@@ -33,7 +33,7 @@ const assurance = () => {
           <a href='https://mighty-reef-11767.herokuapp.com/' target='_blank' rel='noreferrer'>
             <button className='px-8 py-2 mt-4 mr-8'>Demo</button>
           </a>
-          <a href='https://github.com/Jiryeah/FiMi-QA_Application' target='_blank' rel='noreferrer'>
+          <a href='https://github.com/ZairesDev/FiMi-QA_Application' target='_blank' rel='noreferrer'>
             <button className='px-8 py-2 mt-4'>Source Code</button>
           </a>
         </div>
