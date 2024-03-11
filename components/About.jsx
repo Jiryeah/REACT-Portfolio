15df732cab315afa3344c53dcfe49cd7a97283c4
Faissal <faissal.shabani@ideaentity.com>
2024-03-11 12:42:41 -0500

    updated the about me

--- components/About.jsx
+++ components/About.jsx
@@ -10,19 +10,37 @@ const About = () => {
         <div className='col-span-2'>
           <p className='uppercase text-xl tracking-widest text-blue-500'>About</p>
           <h2 className='py-4'>Who I am</h2>
-          <p className='py-2 text-gray-600'>
+          <p className='py-2 text-gray-600 italic text-lg'>
             The <span className='text-black'>thrill</span> of problem solving
           </p>
           <p className='py-2 text-gray-600'>
-            I received a certificate in Full Stack Web Development from the University of Texas at
-            San Antonio. I have been able to make interactive websites that function on all devices
-            and platforms through utilizing the MERN stack. However, I have since learned that
-            back-end logic, seems to be my strong suit; therefore, I learned Java and began to use
-            Spring Boot to further develop my "tool box". The same curiosity that drew me into this
-            field, has humbled me by it's never ending evolution in knowledge and growth. I intend
-            to solidify my back-end foundational skills by learning: AWS, Docker, and TypeScript.
-            One must never abstain from learning, because our time is finite while knowledge is
-            infinite.
+          Hey there! <span className='font-bold text-black italic'>The truth is, I'm obsessed with solving problems that benefit others. So, although I'm grateful that you're about to read my mini-bibliography below, I just like to solve problems that help people first and foremost. </span>
+          
+          I’ve been around the software block for a while, dabbling in front-end wizardry. But you know what? Back-end tech has always whispered sweet nothings to me. So, I’m ready to wade into those server-side waters and broaden my horizons. Let’s build some magic that inspires! 🚀
+          </p>
+          <h3>MERN Stack Foundation: </h3>
+          <p className='py-2 text-gray-600'>
+          I’ve crafted interactive websites that seamlessly function across all devices and platforms using the MERN (MongoDB, Express.js, React, Node.js) stack.
+          Front-end development was my starting point, but I soon realized that my true passion lies in the intricate world of back-end logic.
+          </p>
+          <h3>SharePoint Backends: </h3>
+          <p className='py-2 text-gray-600'>
+          Professionally, I’ve had the privilege of designing backends that are structured within SharePoint. Collaborating with teams, I’ve built efficient workflows and integrated custom solutions.
+          SharePoint’s complexity has challenged me, but it’s also been immensely rewarding.
+          </p>
+          <h3>TypeScript Enthusiast: </h3>
+          <p className='py-2 text-gray-600'>
+          TypeScript has become my trusted companion. Its static typing and enhanced tooling have elevated my development process
+          when utilizing it within React to ensure fluid production level application.
+          </p>
+          <h3>Current Endeavors: </h3>
+          <p className='py-2 text-gray-600'>
+          My learning journey continues! I’m currently diving into C# and exploring the vast .NET Framework.
+          AWS and Docker are next on my list—I’m eager to master cloud services and containerization.
+          </p>
+          <h3> A Never-Ending Quest: </h3>
+          <p className='py-2 text-gray-600'>
+          I firmly believe that learning is a lifelong pursuit. Our time may be finite, but knowledge is infinite. So, I remain curious, humble, and committed to evolving alongside the ever-changing tech landscape.
           </p>
           <Link href='/#projects'>
             <p className='py-2 text-gray-600 underline cursor-pointer'>
